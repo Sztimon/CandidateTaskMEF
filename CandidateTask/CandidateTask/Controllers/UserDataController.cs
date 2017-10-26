8c665f420ba28297f111b05f3ca400986cf406da
Sztimon <szymon.kropisz@gmail.com>
2017-10-26 14:35:13 +0200

    MVC first changes as previous project.

---
+++ CandidateTask/CandidateTask/Controllers/UserDataController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataModel;
+using DataModel.Models;
+
+namespace CandidateTask.Controllers
+{
+    public class UserDataController : Controller
+    {
+        // GET: UserData
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUserData(UserData newUser)
+        {
+            List<UserData> listOfUsers = (List<UserData>)Session["UsersData"];
+            if (listOfUsers == null)
+            {
+                listOfUsers = new List<UserData>();
+            }
+            listOfUsers.Add(newUser);
+            Session["UsersData"] = listOfUsers;
+            return RedirectToAction("Index", "UserData");
+        }
+
+        [HttpPost]
+        public ActionResult SaveUserData()
+        {
+            List<UserData> listOfUsers = (List<UserData>)Session["UsersData"];
+            if (listOfUsers != null)
+            {
+                SaveUserData saveUserData = new SaveUserData();
+                saveUserData.SaveData(listOfUsers);
+                Session["UsersData"] = null;
+                listOfUsers = null;
+            }
+            return RedirectToAction("Index", "UserData");
+        }
+    }
+}
--- CandidateTask/CandidateTask/Global.asax.cs
+++ CandidateTask/CandidateTask/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using DataModel.App_Start;
 
 namespace CandidateTask
 {
@@ -16,6 +18,7 @@ namespace CandidateTask
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Mapper.Initialize(cfg => cfg.AddProfile<MappingProfile>());
         }
     }
 }
